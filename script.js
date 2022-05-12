ebdec5ed2a23fdec9b599b4a762cddd7620a2075
Adrian Ionut Zaharia <addyionut@gmail.com>
2022-05-12 16:21:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ function savePaste() {
 	let title = document.getElementById("title").value;
 	pasteBin.push(title + " : " + input);
 	console.log(pasteBin);
-	document.getElementById("displayPaste").innerHTML = pasteBin.join("<br>") + "<br>";;
+	document.getElementById("displayPaste").innerHTML = pasteBin.join("<br>") + "<br>";
 }
+
+function editPaste() {
+	console.log(pasteBin[pasteBin.length - 1]);
+	document.getElementById("newPaste").innerHTML = pasteBin[0];
+}
+
+function deletePaste() {
+	pasteBin = [];
+	console.log(pasteBin);
+	document.getElementById("displayPaste").innerHTML = pasteBin;
+}
+
 
